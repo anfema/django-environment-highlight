401692df1565e7a9bb5b08b705b20475044b02da
f.plett <f.plett@anfe.ma>
2020-10-05 14:59:33 +0200

    Added settings check and made MANIFEST.in uppercase

--- environment_highlight/apps.py
+++ environment_highlight/apps.py
@@ -1,5 +1,14 @@
 from django.apps import AppConfig
+from django.core.exceptions import ImproperlyConfigured
+from django.conf import settings
 
 
 class EnvironmentHighlightConfig(AppConfig):
     name = 'environment_highlight'
+
+    try:
+        active_environment = settings.ENVIRONMENT_HIGHLIGHT['ACTIVE_ENVIRONMENT']
+        environment_color = settings.ENVIRONMENT_HIGHLIGHT['ENVIRONMENTS'][active_environment]['color']
+        environment_name = settings.ENVIRONMENT_HIGHLIGHT['ENVIRONMENTS'][active_environment]['name']
+    except (AttributeError, KeyError) as e:
+        raise ImproperlyConfigured("Setting ENVIRONMENT_HIGHLIGHT missing or incorrect: {}".format(e))
